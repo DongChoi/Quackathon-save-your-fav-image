5ddb6b43a4398df604f40d9fa777d2a0fa090afc
Dave McConeghy <david.mcconeghy@gmail.com>
2023-03-04 15:44:40 -0500

    Adds localStorage changes to id: url

--- src/Home.js
+++ src/Home.js
@@ -4,37 +4,43 @@ import axios from "axios";
 const Home=()=>{
     const [images, setImages] = useState([]);
     //I don't think we need a state variable for this because in the pinned page, we are looping through what is in localStorage
-  const [userImages, setUserImages] = useState([]);
+    const [userImages, setUserImages] = useState([]);
 
-  const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(function fetchImagesOnLoad() {
-    async function fetchImages() {
-      axios.get("https://api.unsplash.com/photos/random?count=5&client_id=h-JqYpjiPbIbpYbTW9E3yluBUrgU1tCjb2ysGQqM-O0")
-            .then(response=>{
-              setImages(response.data)
-              setIsLoading(false);
-            });
-  }
-    fetchImages();
-  }, []);
+    useEffect(function fetchImagesOnLoad() {
+      async function fetchImages() {
+        axios.get("https://api.unsplash.com/photos/random?count=5&client_id=qP9ixS0i7zK6raD2pGns_MFlRsvZ_z2c-ZhD_eIDLgc")
+              .then(response=>{
+                setImages(response.data)
+                setIsLoading(false);
+              });
+    }
+      fetchImages();
+    }, []);
 
   function handlePinClick(src) {
+    console.log("src id:", src.id);
+    const sourceId = src.id
+    
     // A user's pinned images are stored in localStorage as an array of objects
     // [{id: "123", url: "https://images.unsplash.com/photo-123"}, {id: "456", url: "https://images.unsplash.com/photo-456"}]
 
     // Does the user have any pinned images?
     if (localStorage.getItem("userImages") === null) {
+      console.debug("No pinned images found in localStorage. Creating a userImages object in localStorage");
 
       // If not, create an array with the new image
-      localStorage.setItem("userImages", JSON.stringify([{id:src.id, url:src.urls.small}, ...userImages]));
+      let updatedPinnedImages = { [sourceId] : src.urls.regular, ...userImages}
+      localStorage.setItem("userImages", JSON.stringify(updatedPinnedImages));
     } else {
-
-      // If so, look for the image's id in localStorage
+      // If so, look for the image's id in localStorage object.
+      // If it's not there, add it.
       if (localStorage.getItem(src.id) === null){
-        localStorage.setItem("userImages", JSON.stringify([{id:src.id, url:src.urls.regular}, ...userImages]));     
+        let updatedPinnedImages = { [sourceId] : src.urls.regular, ...userImages}
+        localStorage.setItem("userImages", JSON.stringify(updatedPinnedImages));     
       } else {
-        localStorage.removeItem(src.id);
+        localStorage.removeItem([sourceId]);
         console.log("removed item", src.id)
 
       }
@@ -43,8 +49,8 @@ const Home=()=>{
 
     
     //I don't think we need a state variable for this because in the pinned page, we are looping through what is in localStorage
-    setUserImages([{id:src.id, url:src.urls.regular}, ...userImages]);
-    console.log(userImages);
+    setUserImages([{[sourceId]: src.urls.regular}, ...userImages]);
+    console.log("UserImage State :", userImages);
   }
     return(
         <div>
