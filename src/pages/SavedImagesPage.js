77a3dd33af29fee2e2ae59f8e4d734e6378f8124
Andrew Choi <94206466+DongChoi@users.noreply.github.com>
2023-03-05 21:54:53 -0800

    Update SavedImagesPage.js

--- src/pages/SavedImagesPage.js
+++ src/pages/SavedImagesPage.js
@@ -1,8 +1,11 @@
 import Image from "../components/Image/Image.js";
 
+
+//Props Handed down from App -> RouteList -> SavedImagesPage
 function SavedImagesPage({ images, handlePin, handleUnpin }) {
+  
+  //Turns SavedImages into array of Objects to reuse Image.js Component
   function loopThroughObject(imagess) {
- 
     const keys = Object.keys(imagess);
     const arrayImages = keys.map((key) => {
       return imagess[key];
